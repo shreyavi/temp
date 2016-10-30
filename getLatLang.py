674985dbb69aa765b9cbe358110d75431138643f
shreyavi <shreyavi@usc.edu>
2016-10-30 00:30:54 -0700

    First commit

--- getLatLang.py
+++ getLatLang.py
@@ -1,3 +1,6 @@
 import geocoder
+self.send_header("Access-Control-Allow-Origin","*");
+self.send_header("Access-Control-Expose-Headers: Access-Control-Allow-Origin");
+self.send_header(("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 g = geocoder.google('Mountain View, CA')
 return g.latlng
