6d547c9a605081d3be52de7090ff4d0c9ae40fd0
shreyavi <shreyavi@usc.edu>
2016-10-30 00:46:18 -0700

    First commit

--- getLatLang.py
+++ getLatLang.py
@@ -1,4 +1,28 @@
-print "Access-Control-Allow-Origin: *"
-import geocoder
-g = geocoder.google('Mountain View, CA')
-return g.latlng
+class WebHandler(http.server.BaseHTTPRequestHandler):
+
+    def parse_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers['content-type'])
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers['content-length'])
+            postvars = urllib.parse.parse_qs(self.rfile.read(length),
+                                             keep_blank_values=1)
+        else:
+            postvars = {}
+        return postvars
+
+    def do_POST(self):
+        postvars = self.parse_POST()
+
+        print(postvars)
+
+        # reply with JSON
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*");
+		self.send_header("Access-Control-Expose-Headers", "Access-Control-Allow-Origin");
+		self.send_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        self.end_headers()
+        json_response = json.dumps({'test': 42})
+        self.wfile.write(bytes(json_response, "utf-8"))
